8999848f045c699389ef57a858bb56a68d4bf1bb
Juan Manuel Ros <jmrm_epsfactoria@iecisa.com>
2017-07-26 18:59:21 +0200

    estructura base y estilos limpios. Falta implementar parciales de sass&itcss

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from '../assets/img/logo.svg';
 
-import './styles/index.scss';
+// import '../styles/index.scss';
 
 class App extends Component {
   render() {
